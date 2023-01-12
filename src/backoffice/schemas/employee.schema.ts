b0dade1c5ea48148ffb8aa70f60c455fabcd9d48
franciscocesar <francisco.mateus@souunit.com.br>
2023-01-12 17:31:23 -0300

    feat: create employee schema

---
+++ src/backoffice/schemas/employee.schema.ts
@@ -0,0 +1,55 @@
+import * as mongoose from 'mongoose';
+
+export const EmployeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    default: true,
+    trim: true,
+    index: {
+      unique: true,
+    },
+  },
+  document: {
+    type: String,
+    required: true,
+    trim: true,
+    index: {
+      unique: true,
+    },
+  },
+  hiringDate: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+    index: {
+      unique: true,
+    },
+  },
+  registrationNumber: {
+    type: Number,
+    required: true,
+    index: {
+      unique: true,
+    },
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
