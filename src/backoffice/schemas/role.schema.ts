abab1014fb3e1de2c659e454bfad26c348b50cf9
franciscocesar <francisco.mateus@souunit.com.br>
2023-01-13 16:16:57 -0300

    fix: adjust array on role schema

--- src/backoffice/schemas/role.schema.ts
+++ src/backoffice/schemas/role.schema.ts
@@ -1,16 +1,12 @@
 import * as mongoose from 'mongoose';
 
 export const RoleSchema = new mongoose.Schema({
-  roles: [
-    {
-      name: {
-        type: String,
-        required: true,
-        trim: true,
-        index: {
-          unique: true,
-        },
-      },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    index: {
+      unique: true,
     },
-  ],
+  },
 });
