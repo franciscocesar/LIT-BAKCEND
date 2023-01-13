39cb02474e499bb168279d187647db985e10fc13
franciscocesar <francisco.mateus@souunit.com.br>
2023-01-13 16:17:35 -0300

    feat: update employee func and update password

--- src/backoffice/services/employee.service.ts
+++ src/backoffice/services/employee.service.ts
@@ -1,15 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UpdataPasswordDto } from '../dtos/employee/update-password.dto';
+import { UpdateEmployeeDto } from '../dtos/employee/update.employee.dto';
 import { Employee } from '../models/employee.model';
+import { requiredFildsAlreadyExist } from '../shared/requiredFiled';
 
 @Injectable()
 export class EmployeeService {
   constructor(
     @InjectModel('Employee') private readonly model: Model<Employee>,
   ) {}
-  async create(data: Employee) {
+
+  async createEmployee(data: Employee) {
+    for (const field of requiredFildsAlreadyExist(data)) {
+      const fieldAlreadyExist = await this.model.find(field);
+
+      if (fieldAlreadyExist.length > 0) {
+        throw new HttpException(
+          'Funcionário já cadastrado',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    }
+
     const employee = new this.model(data);
     return await employee.save();
+  }
+
+  async findAllEmployees() {
+    return await this.model.find({}, '-password').exec();
+  }
+
+  async updateEmployee(
+    document: string,
+    data: UpdateEmployeeDto,
+  ): Promise<Employee> {
+    return await this.model.findOneAndUpdate({ document }, data);
+  }
+
+  async updatePassword(document: string, data: UpdataPasswordDto) {
+    return await this.model.findOneAndUpdate({ document }, data);
   }
 }
