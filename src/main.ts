a149dc9a482baa3bbecd2a57699074e369e735ca
franciscocesar <francisco.mateus@souunit.com.br>
2023-01-13 16:11:02 -0300

    feat: add sawgger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('LIT-BACKEND')
+    .setDescription('BACK-END Feito para teste de contratação da LIT')
+    .setVersion('1.0.0')
+    .addTag('LIT')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
