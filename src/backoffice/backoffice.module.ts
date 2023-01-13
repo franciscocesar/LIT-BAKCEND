89135cd7955a5801cb4a6bc0def71cf68f0cde6d
franciscocesar <francisco.mateus@souunit.com.br>
2023-01-13 16:11:19 -0300

    feat: config dotenv files

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose/dist';
 import { EmployeeController } from './controllers/employees.controller';
+import { RoleController } from './controllers/role.controller';
 import { EmployeeSchema } from './schemas/employee.schema';
 import { RoleSchema } from './schemas/role.schema';
 import { EmployeeService } from './services/employee.service';
+import { RoleService } from './services/role.service';
 
 @Module({
   imports: [
@@ -17,8 +20,12 @@ import { EmployeeService } from './services/employee.service';
         schema: RoleSchema,
       },
     ]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
   ],
-  controllers: [EmployeeController],
-  providers: [EmployeeService],
+  controllers: [EmployeeController, RoleController],
+  providers: [EmployeeService, RoleService],
 })
 export class BackofficeModule {}
