37d82c334a05c871395fda942f0c6b525e238c8b
franciscocesar <francisco.mateus@souunit.com.br>
2023-01-13 16:13:59 -0300

    feat: update employeer and passoword and create encrypt password

--- src/backoffice/controllers/employees.controller.ts
+++ src/backoffice/controllers/employees.controller.ts
@@ -2,12 +2,21 @@ import {
   Body,
   Controller,
   Get,
+  HttpException,
+  HttpStatus,
+  Param,
   Post,
   Put,
   UseInterceptors,
 } from '@nestjs/common';
+import { Md5 } from 'md5-typescript';
+import { model } from 'mongoose';
 import { ValidatorInterceptor } from 'src/interceptors/validator.interceptor';
-import { CreateEmployeeContract } from '../contracts/employee.contracts';
+import { CreateEmployeeContract } from '../contracts/employee/create-employee.contracts';
+import { UpdateEmployeeContract } from '../contracts/employee/update-employee.contract';
+import { UpdatePasswordContract } from '../contracts/employee/update-password.contrat';
+import { UpdataPasswordDto } from '../dtos/employee/update-password.dto';
+import { UpdateEmployeeDto } from '../dtos/employee/update.employee.dto';
 import { ResultDto } from '../dtos/result.dto';
 import { Employee } from '../models/employee.model';
 import { EmployeeService } from '../services/employee.service';
@@ -17,36 +26,81 @@ export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
 
   @Get()
-  get() {
-    return new ResultDto(null, true, {}, null);
+  async get() {
+    const employee = await this.employeeService.findAllEmployees();
+    return new ResultDto(null, true, employee, null);
   }
 
   @Post()
   @UseInterceptors(new ValidatorInterceptor(new CreateEmployeeContract()))
   async post(@Body() model: Employee) {
-    const employee = await this.employeeService.create(
-      new Employee(
-        model.name,
-        model.email,
-        model.document,
-        model.hiringDate,
-        model.registrationNumber,
-        model.role,
-        model.password,
-        model.active,
-      ),
-    );
+    try {
+      const password = await Md5.init(
+        `${model.password}${process.env.SALT_KEY}`,
+      );
+      const employee = await this.employeeService.createEmployee(
+        new Employee(
+          model.name,
+          model.email,
+          model.document,
+          model.hiringDate,
+          model.registrationNumber,
+          model.role,
+          password,
+          model.active,
+        ),
+      );
 
-    return new ResultDto(
-      'Funcionário criado com sucesso!',
-      true,
-      employee,
-      null,
-    );
+      return new ResultDto(
+        'Funcionário criado com sucesso!',
+        true,
+        employee,
+        null,
+      );
+    } catch (error) {
+      throw new HttpException(
+        new ResultDto('Erro ao realizar cadastro', false, null, error),
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  @Put()
-  put(@Body() body) {
-    return new ResultDto('Funcionário alterado com sucesso!', true, body, null);
+  @Put(':document')
+  @UseInterceptors(new ValidatorInterceptor(new UpdateEmployeeContract()))
+  async update(@Param('document') document, @Body() model: UpdateEmployeeDto) {
+    try {
+      await this.employeeService.updateEmployee(document, model);
+      return new ResultDto(null, true, model, null);
+    } catch (error) {
+      throw new HttpException(
+        new ResultDto(
+          'Erro ao atualizar cadastro, verifique se existe outro usuário com mesmos campos',
+          false,
+          null,
+          error,
+        ),
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Put('update-password/:document')
+  @UseInterceptors(new ValidatorInterceptor(new UpdatePasswordContract()))
+  async updatePassowrd(
+    @Param('document') document,
+    @Body() model: UpdataPasswordDto,
+  ) {
+    try {
+      const password = await Md5.init(
+        `${model.password}${process.env.SALT_KEY}`,
+      );
+      await this.employeeService.updatePassword(document, { password });
+      return new ResultDto('Senha alterada com sucesso', true, model, null);
+    } catch (error) {
+      throw new HttpException(
+        new ResultDto('Erro ao atualizar cadastro a senha', false, null, error),
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
